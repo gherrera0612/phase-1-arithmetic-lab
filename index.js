18a9ac9fac141f83e8314274f082229a7ef6bcfd
Trey Herrera <therrera152@gmail.com>
2022-02-27 09:56:02 -0600

    commit tree ea82508c073465c9b9a5304674f7c25dcf930b8f

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
-// Write your code here
+var multiply;
+var num1 = 31;
+var num2 = 2;
+var multiply = num1 * num2;
+//multiply = 62;
+
+
+
+var random;
+var min = 1;
+var max = 20;
+var random = Math.floor(Math.random() * (max-min)) + min;
+
+
+
+var mod;
+var num3 = 10;
+var num4 = 6;
+var mod = num3 % num4;
+
